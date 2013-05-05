12e456428a4e41ead53c8e2fdee776ef53619e78
Son-Huy TRAN <sonhuytran@gmail.com>
2013-05-05 22:12:17 +0200

    02.01.LifeCycleEvents committed

--- 02.01.LifeCycleEvents/Default.aspx.cs
+++ 02.01.LifeCycleEvents/Default.aspx.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Web.UI;
 
-public partial class _Default : System.Web.UI.Page
+public partial class _Default : Page
 {
+    #region Page's LifeCycleEvents
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        Debug.WriteLine("Page_Load");
     }
+
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Page_PreInit");
+    }
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Page_Init");
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Page_PreRender");
+    }
+
+    protected void Page_Unload(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Unload");
+    }
+
+    #endregion Page's LifeCycleEvents
 }
